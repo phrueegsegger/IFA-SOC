c0491242c51c3e81c767d8ba300f3fd75bf8e39c
Rüegsegger Philipp <philipp.rueegsegger@gmail.com>
2018-09-20 06:17:50 -0400

    after block 1

--- Block 1/tool_deauth_pr.sh
+++ Block 1/tool_deauth_pr.sh
@@ -6,3 +6,25 @@
 ################### From the freecybersecurity.org Project    ###################
 ################### Thanks to the community!                  ###################
 #################################################################################
+
+
+# airmon-ng start wlan0
+# airmon-ng check kill
+# airodump-ng wlan0mon
+
+start-airmon() {
+    airmon-ng start wlan0
+    airmon-ng check kill
+    clear
+}
+
+
+#main
+start-airmon
+
+
+filter="((wlan.fc.type_subtype==0x04)&&!(wlan.tag.data=="'""'"))"
+
+# tshark -i wlan0mon -T fields -e frame.time_relative \
+#       -e wlan.sa -e wlan.da -e wlan_radio.signal_dbm \
+#       -e wlan.ssid -Y $filter
